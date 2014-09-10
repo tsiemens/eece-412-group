a9af8f84e11d0fd600375008aa69939a22f7a5e0
Trevor Siemens <USER@DOMAIN>
2014-09-10 14:51:29 -0700

    added tk to reqs, and a test window

--- assignment3/assignment3.py
+++ assignment3/assignment3.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+#!/usr/bin/python2.7
+from Tkinter import *
+
+def on_test_button_pressed():
+    print("Button pressed")
+
+def main():
+    print("Hello World!") 
+    root = Tk()
+    # a test button
+    test_button = Button(root, text='Test',
+                         command=on_test_button_pressed)
+    test_button.grid(row=0, column=0)
+    test_button.pack(pady=20, padx = 20)
+   
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
