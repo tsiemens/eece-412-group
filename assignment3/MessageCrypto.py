c283adef1dbb80d9d588bc4de67a720ec429508a
Zachary -L <zmoshansky@gmail.com>
2014-10-05 21:08:14 -0700

    Crypto module implementing an AES cryptographically secure cryptosystem

---
+++ assignment3/MessageCrypto.py
@@ -0,0 +1,38 @@
+from Crypto.Cipher import AES
+import os, base64
+
+
+class MessageCrypto(object):
+    def __init__(self, key):
+        self.key = key
+
+    def encrypt(self, plaintext):
+        """ Returns ciphertext prepended by IV - Base64 Encoded """
+        iv = os.urandom(16)
+        crypto_system = self._init_crypto_system(iv)
+        return base64.b64encode(iv + crypto_system.encrypt(self._pad_message(plaintext)))
+
+    def decrypt(self, ciphertext):
+        """ Returns plaintext from ciphertext, prepended by IV - Base64 Encoded """
+        binary_ciphertext = base64.b64decode(ciphertext)
+        crypto_system = self._init_crypto_system(binary_ciphertext[:16])
+        plaintext = crypto_system.decrypt(binary_ciphertext[16:])
+        return self._unpad_message(plaintext)
+
+    def _init_crypto_system(self, iv):
+        """ Initializes a cryptosystem for encryption and decryption """
+        return AES.new(self.key, AES.MODE_CBC, iv)
+
+    def _pad_message(self, plaintext):
+        """ Pads plaintext with newline characters, to a round 16 bytes """
+        pad_count = len(plaintext) % 16
+        padding = '\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
+
+        if (pad_count != 0):
+            return plaintext + padding[:(16 - pad_count)]
+        else:
+            return plaintext
+
+    def _unpad_message(self, plaintext):
+        """ Removes trailing newline characters """
+        return plaintext.rstrip()
---
+++ assignment3/tests/TestMessageCrypto.py
@@ -0,0 +1,23 @@
+import unittest, hashlib, hmac
+from assignment3.MessageCrypto import *
+
+class TestMessageCrypto(unittest.TestCase):
+
+    def setUp(self):
+        self.crypto_system = MessageCrypto('1111111111111111')
+
+    def test_encrypt_generates_new_iv(self):
+        encrypted_message_0 = self.crypto_system.encrypt('testtesttesttest')
+        encrypted_message_1 = self.crypto_system.encrypt('testtesttesttest')
+        self.assertNotEqual(encrypted_message_0, encrypted_message_1)
+
+    def test_message_padding(self):
+        encrypted_message_0 = self.crypto_system.encrypt('test')
+        self.assertNotEqual(encrypted_message_0, None)
+
+    def test_encrypt_decrypt(self):
+        encrypted_message_0 = self.crypto_system.encrypt('test')
+        self.assertEqual(self.crypto_system.decrypt(self.crypto_system.encrypt('test')), 'test')
+
+if __name__ == '__main__':
+    unittest.main()
